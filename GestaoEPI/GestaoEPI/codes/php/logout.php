902f739e760e85bebfa024db53052e7d14ad145a
caiquegs <162234979+caiquegs@users.noreply.github.com>
2025-05-18 14:09:13 -0300

    Delete GestaoEPI directory

--- GestaoEPI/GestaoEPI/codes/php/logout.php
+++
@@ -1,5 +0,0 @@
-<?php
-      session_start(); // Inicia a sessão
-      session_destroy(); // Destrói a sessão limpando todos os valores salvos
-      header("Location: http://localhost/gestaoepi/codes/index.html"); exit; // Redireciona o visitante
-  ?>
